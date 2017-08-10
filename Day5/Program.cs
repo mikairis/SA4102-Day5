7538334a9b43469f13e8936be36837dbbd07c7e5
mikairis <thong.michael@gmail.com>
2017-08-10 09:34:32 +0800

    Quiz

--- Day5/Program.cs
+++ Day5/Program.cs
@@ -10,6 +10,30 @@ namespace Day5
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("***************This is day 5 quiz**************");
+
+            int[,] theArray = new int[,]
+            {
+                {5,3},{2,9},{2,9},
+                {5,1},{7,1},{8,3},
+                {0,3},{2,2},{9,7}
+            };
+
+            int count = 0;
+            Start:
+            Console.WriteLine("Please enter a number (0-9): ");
+            int input = Convert.ToInt32(Console.ReadLine());
+            
+            foreach (int i in theArray)
+            {
+                if (i == input)
+                {
+                    count++;
+                }
+            }
+            Console.WriteLine("The number " + input + " appeared " + count + " times!");
+            Console.ReadKey();
+            goto Start;
         }
     }
 }
